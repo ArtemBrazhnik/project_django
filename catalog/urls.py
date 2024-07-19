269a6edccb0839234ba1674748c2e27f770f346f
Artem Brazhnik <dambldor55@gmail.com>
2024-07-19 19:13:35 +0300

    шаблоны

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('catalog/<int:pk>/', product_detail, name='product_detail'),
+    path('catalog/', product_list, name='product_list'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base.html')
 
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
